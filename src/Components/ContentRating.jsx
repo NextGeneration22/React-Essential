9d087a05b563f85185cbd91438165f05e7c5b508
NextGeneration22 <ayoubejraidi@gmail.com>
2024-12-23 13:22:59 -0500

    add the firs react practice project

--- src/Components/ContentRating.jsx
+++ src/Components/ContentRating.jsx
@@ -1,16 +1,52 @@
-
 import React, { Component } from 'react';
 import './ContentRating.css';
 
 class ContentRating extends Component {
   constructor() {
     super();
+    this.state = {
+      likes: 0,
+      dislikes: 0,
+      totalRating: 0
+    };
   }
+
+  handlelike = () => {
+    this.setState(
+      (prevState) => ({ likes: prevState.likes + 1 }),
+      this.TotalRating
+    );
+  };
+
+  handledislike = () => {
+    this.setState(
+      (prevState) => ({ dislikes: prevState.dislikes + 1 }),
+      this.TotalRating 
+    );
+  };
+
+  TotalRating = () => {
+    this.setState((prevState) => ({
+      totalRating: prevState.likes + prevState.dislikes
+    }));
+  };
+
   render() {
     return (
-     <>
-     <h1>Text Content Rating</h1>
-     </>
+      <div className='content-rating'>
+        <p>
+          "Learning is not for free; you need to pay attention"
+        </p>
+        <div className='rating-buttons'>
+          <button className="like-button" onClick={this.handlelike}>
+            Like ({this.state.likes})
+          </button>
+          <button className="dislike-button" onClick={this.handledislike}>
+            Dislike ({this.state.dislikes})
+          </button>
+          <p>Total Rating: {this.state.totalRating}</p>
+        </div>
+      </div>
     );
   }
 }
